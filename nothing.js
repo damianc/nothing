d5a0da5bc4e6bb781886abd28df792b180fe14f4
damianc <damianczapiewski@gmail.com>
2018-08-28 14:40:41 +0200

    normalize indentations

--- nothing.js
+++ nothing.js
@@ -1,17 +1,17 @@
 Object.defineProperty(Array.prototype, 'nothing', {
-	get() {
-		var that = this;
+    get() {
+        var that = this;
 
-		return {
-			atTheBeginning: function () {
-				return that.unshift(that.shift());
-			},
-			atTheEnd: function () {
-				return that.push(that.pop());
-			},
+        return {
+            atTheBeginning: function () {
+                return that.unshift(that.shift());
+            },
+            atTheEnd: function () {
+                return that.push(that.pop());
+            },
             atThePosition: function (index) {
                 return that.splice(index, 0, that.splice(index, 1)[0]);
             }
-		};
-	}
+        };
+    }
 });
