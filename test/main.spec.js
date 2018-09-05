c3595e911fc2826ca1bfae0e16a32865cc273b38
damianc <damianczapiewski@gmail.com>
2018-09-06 00:53:22 +0200

    add unit tests for internal calls spies

--- test/main.spec.js
+++ test/main.spec.js
@@ -1,5 +1,5 @@
-var expect = require('expect');
 var nothing = require('../src/nothing');
+var [should] = [require('should'), require('should-spies')];
 
 describe('# OBJECT OPERATIONS', function () {
 	it('should do nothing at the beginning of an array', function () {
@@ -7,7 +7,7 @@ describe('# OBJECT OPERATIONS', function () {
 		var arrCopy = Array.from(arrOriginal);
 		
 		arrCopy.nothing.atTheBeginning();
-		expect(arrCopy.join('')).toEqual(arrOriginal.join(''));
+		arrCopy.should.be.eql(arrOriginal);
 	});
 
 	it('should do nothing in the middle of an array', function () {
@@ -15,7 +15,7 @@ describe('# OBJECT OPERATIONS', function () {
 		var arrCopy = Array.from(arrOriginal);
 		
 		arrCopy.nothing.atThePosition(Math.floor(arrCopy.length / 2));
-		expect(arrCopy.join('')).toEqual(arrOriginal.join(''));
+		arrCopy.should.be.eql(arrOriginal);
 	});
 
 	it('should do nothing at the end of an array', function () {
@@ -23,6 +23,40 @@ describe('# OBJECT OPERATIONS', function () {
 		var arrCopy = Array.from(arrOriginal);
 		
 		arrCopy.nothing.atTheEnd();
-		expect(arrCopy.join('')).toEqual(arrOriginal.join(''));
+		arrCopy.should.be.eql(arrOriginal);
+	});
+});
+
+describe('# INTERNAL OPERATIONS', function () {
+	it('should call unshift() and shift() methods of the Array.prototype object', function () {
+		var arr = [1, 2, 3, 4];
+		var spyUnshift = should.spy.on(arr, 'unshift');
+		var spyShift = should.spy.on(arr, 'shift');
+
+		arr.nothing.atTheBeginning();
+
+		spyUnshift.should.be.calledWith(1).and.be.calledOn(arr);
+		spyShift.should.be.calledOn(arr);
+	});
+
+	it('should call splice() method of the Array.prototype object', function () {
+		var arr = [1, 2, 3, 4];
+		var spySplice = should.spy.on(arr, 'splice');
+
+		arr.nothing.atThePosition(2);
+
+		spySplice.should.be.calledOn(arr);
+		spySplice.should.have.callCount(2);
+	});
+
+	it('should call push() and pop() methods of the Array.prototype object', function () {
+		var arr = [1, 2, 3, 4];
+		var spyPush = should.spy.on(arr, 'push');
+		var spyPop = should.spy.on(arr, 'pop');
+
+		arr.nothing.atTheEnd();
+
+		spyPush.should.be.calledWith(4).and.be.calledOn(arr);
+		spyPop.should.be.calledOn(arr);
 	});
 });
